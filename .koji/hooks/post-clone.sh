84209a5ab86376479a654a8c12376a7b56aa7538
jones <jones@koji-projects.com>
2019-04-09 23:12:14 +0000

    maybe this

--- .koji/hooks/post-clone.sh
+++ .koji/hooks/post-clone.sh
@@ -1,20 +1,3 @@
 #!/bin/sh
 
-npx create-react-app frontend
-
-rm -rf tasks
-rm -rf packages
-rm -rf test
-rm -rf .github
-rm -rf docusaurus
-rm screencast-error.svg
-rm screencast.svg
-rm CHANGELOG.md
-rm CHANGELOG-0.x.md
-rm netlify.toml
-rm CONTRIBUTING.md
-rm LICENSE
-rm CODE_OF_CONDUCT.md
-rm lerna.json
-rm CHANGELOG-1.x.md
-rm package.json
+echo ahahahahaha
