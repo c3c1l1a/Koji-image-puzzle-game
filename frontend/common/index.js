b04819b1dadf425848dcf8a52c353257dc876764
jones <jones@koji-projects.com>
2019-04-29 21:09:40 +0000

    getting somewhere

--- frontend/common/index.js
+++ frontend/common/index.js
@@ -12,14 +12,34 @@
  *   here.
  */
 
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from '.internals/serviceWorker';
+import * as p5 from './p5.min.js';
+import wrapConsole from 'koji_utilities/wrapConsole';
+import App from 'pages/HomePage';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const { koji } = process.env;
+window.koji = koji;
 
+wrapConsole();
+console.log('[koji] frontend started');
+
+// Wire debug hooks for theme if in development environment
+if (process.env.NODE_ENV !== 'production') {
+    window.addEventListener('message', ({ data }) => {
+        // Global context injection
+        if (data.action === 'injectGlobal') {
+            const { scope, key, value } = data.payload;
+            window.koji[scope][key] = value;
+        }
+    }, false);
+}
+
+Object.entries(App).map(([name, module]) => {
+  window[name] = module;
+});
+
+new p5();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- frontend/pages/HomePage/index.js
+++ frontend/pages/HomePage/index.js
@@ -1,80 +1,31 @@
-import React from 'react';
-import styled, { keyframes } from 'styled-components';
+let img;
 
-import GlobalContext from 'common/GlobalContext';
-import Request from 'koji_utilities/request';
-import Routes from 'koji_utilities/routes';
-
-const Container = styled.div`
-    background-color: ${({ theme }) => theme.colors.backgroundColor};
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    color: ${({ theme }) => theme.colors.textColor};
-    text-align: center;
-`;
-
-const AppLogoSpin = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const Content = styled.div`
-  padding-bottom: 8px;
-`;
-
-const Link = styled.a`
-  color: ${({ theme }) => theme.colors.linkColor};
-`;
-
-const Icon = styled.div`
-    animation: ${AppLogoSpin} infinite 20s linear;
-    height: 40vmin;
-    width: 60vmin;
-    pointer-events: none;
-    background-image: url(${({ theme }) => theme.images.icon});
-    background-size: cover;
-    margin-bottom: 16px;
-`;
-
-class HomePage extends React.PureComponent {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            response: '',
-        };
-    }
-
-    componentDidMount() {
-        Request(Routes.SampleRoute).then((e) => this.setState({ response: e.response }));
-    }
-
-    render() {
-        return (
-            <Container>
-                <Icon />
-                <Content>{this.state.response}</Content>
-                <Content>{this.context.strings.content}</Content>
-                <Link
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                    {this.context.strings.linkText}
-                </Link>
-            </Container>
-        );
-    }
+function setup() {
+  // make a full screen canvas
+  createCanvas(window.innerWidth, window.innerHeight);
+  img = loadImage(koji.images.mouse); // Load the image
 }
 
-HomePage.contextType = GlobalContext;
+function draw() {
+  // set the background color from the configuration options
+  background(koji.colors.backgroundColor);
 
-export default HomePage;
+  // format our text
+  textSize(24);
+  fill(koji.colors.textColor);
+  textAlign(CENTER);
+
+  // print out our text
+  text(koji.strings.content, window.innerWidth / 2, 100);
+
+  // setup an image to follow our mouse
+  let imageSize = 100;
+  image(img, mouseX - (imageSize / 2), mouseY - (imageSize / 2), imageSize, imageSize);
+}
+
+
+// export our functions so p5 can find them
+export default {
+  setup,
+  draw,
+}
