ee1873fb669e11b66dbafee99e8915f66f0c838c
kumar_abhirup <kumar_abhirup@koji-projects.com>
2019-07-28 00:33:35 +0000

    Make something appear

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,4 +1,4 @@
-/* global p5, Koji */
+/* global Koji, createCanvas, background, textSize, text */
 
 // Load assets
 function preload() {
@@ -7,10 +7,12 @@ function preload() {
 
 // Setup your props
 function setup() {
-    
+    createCanvas(windowWidth, windowHeight)
+    background(255)
 }
 
 // An infinite loop that never ends in p5
 function draw() {
-    
+    textSize(50)
+    text('🤩', width/2 - 22.5, height/2)
 }
