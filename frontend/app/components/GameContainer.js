7352bb9c9c21e0ad9f622ed6aefead599cdf9a5d
c3c1l1a <cesswangumukima@gmail.com>
2019-12-07 13:42:34 +0800

    Added game component that is housing the canvas

--- frontend/app/components/GameContainer.js
+++ frontend/app/components/GameContainer.js
@@ -1,7 +1,18 @@
 import { h, Component } from 'preact';
 
 export default class GameContainer extends Component {
+	componentWillMount() {
+		require('script-loader!app/index.js');
+	}
+
+	commponentDidMount(){
+		this.p5Game = new p5(null, document.getElementById('game-container'));
+	}
+
+	componentWillUnmount() {
+    	this.p5Game.remove();
+	}
 	render(){
-		return (<div>Test</div>);
+		return <div id={'game-container'}></div>;
 	}
 }
